f02b994ec12955c47d335eb2720230efe6ecbd43
yuliia levytska <lupin773311asd@gmail.com>
2025-12-09 10:56:37 +0000

    ав

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: "/oracle-app/",   // <<< ВАЖНО!!!
-  plugins: [react(), svgr()],
+ base: "/",
+   plugins: [react(), svgr()],
 });
 
 
